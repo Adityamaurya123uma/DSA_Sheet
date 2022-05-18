5f24bac15458b181db1d99fd82eec118aebc9f63
Adityamaurya123uma <63842678+Adityamaurya123uma@users.noreply.github.com>
2022-05-18 19:07:13 +0530

    new commit

--- Array/Easy/Search an element in a sorted and pivoted array.cpp
+++ Array/Easy/Search an element in a sorted and pivoted array.cpp
@@ -1,28 +1,60 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
 class Solution{
     public:
-    int search(int a[], int l, int h, int key){
+    int search(int a[], int l, int h, int target){
     //complete the function here
      while(l<=h)
    {
       int mid=(l+h)/2;
-      if(a[mid]==key)
-      return mid;
-      if(a[l]<=a[mid] && a[mid]>=a[h])
+      
+      if(a[mid]==target)
       {
-          if(key>=a[l] && key<a[mid])
-          h=mid-1;
-          else
-          l=mid+1;
+          return mid;
       }
-      else
+      
+      if(a[l]<=a[mid])
       {
-          if(key>a[mid] && key<=a[h])
-          l=mid+1;
-          else
-          h=mid-1;
+          if(a[l]<=target && target<a[mid]){
+              h=mid-1;
+          }
+          else{
+              l=mid+1;
+          }
       }
+      else{
+          if(a[mid]<=target && target<a[l]){
+              l=mid+1;
+          }
+          else{
+              h=mid-1;
+          }
+      }
+      
+     
    }
    return -1;
     
     }
 };
+
+// { Driver Code Starts. 
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int A[n];
+        for(int i = 0; i < n; i++)
+            cin >> A[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(A, 0, n - 1, key) << endl;
+    }
+return 0;
+}  // } Driver Code Ends
